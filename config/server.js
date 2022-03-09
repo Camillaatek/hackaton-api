13fa38ea80a46260c29b448e38f7bad5accd4566
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2022-03-09 14:55:14 +0100

    fix

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['yourkey1', 'yourkey2'],
   },
 });
